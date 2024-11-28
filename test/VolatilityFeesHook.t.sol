b909ec15528f51830f5be8bb1ee33180a625657a
Shahan K <shahan.k.code@gmail.com>
2024-11-28 15:48:03 -0500

    base test

---
+++ test/VolatilityFeesHook.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+import {PoolManager} from "v4-core/PoolManager.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {LPFeeLibrary} from "v4-core/libraries/LPFeeLibrary.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
+import {VolatilityFeesHook} from "../src/VolatilityFeesHook.sol";
+import {TickMath} from "v4-core/libraries/TickMath.sol";
+import {console} from "forge-std/console.sol";
+
+contract TestGasPriceFeesHook is Test, Deployers {
+    using CurrencyLibrary for Currency;
+    using PoolIdLibrary for PoolKey;
+
+    VolatilityFeesHook hook;
+
+	function setUp() public {
+		// TODO
+	}
+}
