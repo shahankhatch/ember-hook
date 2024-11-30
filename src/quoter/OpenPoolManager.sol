344ce1f6b59dadfde5a4f8edf2106bace6674bd1
Shahan K <shahan.k.code@gmail.com>
2024-11-29 23:39:32 -0500

    capture attempts

--- src/quoter/OpenPoolManager.sol
+++ src/quoter/OpenPoolManager.sol
@@ -7,10 +7,21 @@ import {PoolId} from "@uniswap/v4-core/src/types/PoolId.sol";
 import {Pool} from "@uniswap/v4-core/src/libraries/Pool.sol";
 import {BalanceDelta} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
 import {StateLibrary} from "@uniswap/v4-core/src/libraries/StateLibrary.sol";
+import {ProtocolFees} from "@uniswap/v4-core/src/ProtocolFees.sol";
+import {ProxyPoolManager} from "@uniswap/v4-core/src/test/ProxyPoolManager.sol";
 
-contract OpenPoolManager is PoolManager {
-    using StateLibrary for Pool.State;
-    constructor(address initialOwner) PoolManager(initialOwner) {}
+contract OpenPoolManager is ProxyPoolManager {
+    // using Pool for Pool.State;
+    // constructor(address Po) PoolManager(initialOwner) {}
+    constructor(address delegateManager) ProxyPoolManager(delegateManager) {}
+
+    // function getPoolState(PoolId id) public view returns (Pool.State memory) {
+    //     return _pools[id];
+    // }
+
+    // function getPoolBitmap(PoolId id) public view returns (mapping(int16 => uint256) memory) {
+    //     return _pools[id].tickBitmap;
+    // }
 
     // function inner_swap(PoolId id, Pool.SwapParams memory params) public view returns (BalanceDelta swapDelta, uint256 amountToProtocol, uint24 swapFee, Pool.SwapResult memory result) {
     //     return _pools[id].swap(params);
