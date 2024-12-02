2ec33b30d343b3ca45c5bffde03e6fc8f2556dfb
Shahan K <shahan.k.code@gmail.com>
2024-12-02 16:16:20 -0500

    keep sol version

---
+++ src/volatility/VolatilityContract.sol
@@ -0,0 +1,34 @@
+import {IVolatilityContract} from "../interfaces/IVolatilityContract.sol";
+
+contract VolatilityContract is IVolatilityContract {
+    uint256[] public swaps;
+    uint256 public index;
+    uint256 public lastVolatility;
+
+    function init() external override {
+        index = 0;
+        lastVolatility = 0;
+    }
+
+    function setVolatility(uint256 new_volatility) external override {
+        lastVolatility = new_volatility;
+    }
+
+    function addSwap(uint256 swap) external override {
+        lastVolatility += swap;
+    }
+
+    function simulateSwap(uint256 swap) external override returns (uint256) {
+        return index + swap;
+    }
+
+    function sqrt(uint256 value) external view override returns (uint256) {
+        uint256 z = (value + 1) / 2;
+        uint256 y = value;
+        while (z < y) {
+            y = z;
+            z = (value / z + z) / 2;
+        }
+        return y;
+    }
+}
