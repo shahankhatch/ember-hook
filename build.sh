c856f7e6ff87bca61ed6beac573b9f5afad56530
Shahan K <shahan.k.code@gmail.com>
2024-12-07 22:46:19 -0500

    build

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+rm -rf src/brevis
+mkdir -p src/brevis
+
 cp -R -f ../ember-brevis/examples/contracts/contracts/ src/brevis
+
+rm -rf src/brevis/examples/slot
+rm -rf src/brevis/examples/tokenTransfer
+rm -rf src/brevis/examples/tradingvolume
 
 forge build -vvvv --extra-output evm --optimize true --optimizer-runs 1 --no-cache --revert-strings debug --build-info
